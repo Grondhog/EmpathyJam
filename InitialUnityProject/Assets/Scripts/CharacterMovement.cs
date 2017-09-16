41765dbe1558dcfe7d39218663bdee1e36336180
DIGIPEN.EDU\d.reynolds <mtae000@gmail.com>
2017-09-16 13:57:06 -0700

    Added character movement using the WASD keys.

---
+++ InitialUnityProject/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    const float SPEED = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+       Debug.Log("Walking with some tunes in.");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //Print every key pressed
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("Key pressed is: " + Input.inputString);
+        }
+
+        //Move character on a plane
+        transform.Translate(SPEED * Time.deltaTime * Input.GetAxis("Horizontal"),0.0f, SPEED * Time.deltaTime * Input.GetAxis("Vertical"));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision detected with." + other.GetComponent<Collider>().name);
+    }
+}
