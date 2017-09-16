783b980292ed538537e86fd8121c3b8c569dde54
Grondhog <Sgronhovd@gmail.com>
2017-09-16 12:57:54 -0700

    NPC can now handle a blocked navigation node. Started NPCManager.

---
+++ InitialUnityProject/Assets/Scripts/NPCManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour {
+
+    public List<List<Transform>> routes;
+
+    public GameObject npc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- InitialUnityProject/Assets/Scripts/NPCPathing.cs
+++ InitialUnityProject/Assets/Scripts/NPCPathing.cs
@@ -12,22 +12,34 @@ public class NPCPathing : MonoBehaviour {
     public Transform[] destinations;
 
     private int i = 0;
+    private float prevRemainingDistance;
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(destinations[0].position);
         i = (i + 1) % destinations.Length;
+        prevRemainingDistance = agent.remainingDistance;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(i);
-        Debug.Log(agent.remainingDistance + ", " + agent.stoppingDistance);
-		if(agent.remainingDistance <= agent.stoppingDistance)
+        // Debug.Log(agent.remainingDistance + ", " + agent.stoppingDistance);
+        Debug.Log(agent.remainingDistance + ", " + prevRemainingDistance);
+		if(agent.remainingDistance <= agent.stoppingDistance )
         {
             agent.SetDestination(destinations[i++].position);
-            i = i % destinations.Length;
+            i = i % (destinations.Length );
+            
         }
-	}
+        else if(agent.remainingDistance > prevRemainingDistance && prevRemainingDistance > 0 && agent.remainingDistance - prevRemainingDistance < 1)
+        {
+            agent.SetDestination(destinations[i++].position);
+            i = i % (destinations.Length );
+            
+        }
+        Debug.Log(i);
+        prevRemainingDistance = agent.remainingDistance;
+
+    }
 }
