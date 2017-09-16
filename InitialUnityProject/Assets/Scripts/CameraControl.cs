98c6442edcf68b23f56585073d74610c5607fee6
Grondhog <Sgronhovd@gmail.com>
2017-09-16 15:52:51 -0700

    Created school scene template, still empty. Created simple Top Down Camera control.

---
+++ InitialUnityProject/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    Camera Cam;
+    public Transform target;
+
+    public float smoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        Cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 goalPos = target.position;
+        goalPos.y = transform.position.y;
+        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+    }
+}
