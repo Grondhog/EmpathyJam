8d5b5bce4ff8f2c305841c2c950537d4fb60e43f
Grondhog <Sgronhovd@gmail.com>
2017-09-16 11:17:33 -0700

    Adding NavMeshComponent from Unity, NPCNavigation script, and Test Scene

---
+++ InitialUnityProject/Assets/Scripts/NPCPathing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class NPCPathing : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+    public Transform[] destinations;
+
+    private int i = 0;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(destinations[0].position);
+        i = (i + 1) % destinations.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(i);
+        Debug.Log(agent.remainingDistance + ", " + agent.stoppingDistance);
+		if(agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.SetDestination(destinations[i++].position);
+            i = i % destinations.Length;
+        }
+	}
+}
