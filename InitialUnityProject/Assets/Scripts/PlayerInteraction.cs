d1027668dd37df0cbc3e9dd96a9c7c3c98065a68
Grondhog <Sgronhovd@gmail.com>
2017-09-17 13:35:50 -0700

    Adding more music, and initial commit for playerinteraction script.

---
+++ InitialUnityProject/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+
+    public TextBoxManager tbMan;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckInput();
+        //tbMan.bActive = false;
+	}
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //tbMan.bActive = true;
+        }
+    }
+}
